7426f100e6ccb80da9990bec39c5d03a4acd91a6
udaychopade27 <udaychopade27@gmail.com>
2023-12-19 13:17:38 +0530

    Daily Wage calculation is pushed to UC2

---
+++ EmployeeWageComputations/DailyEmployeeWage.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.uc1;
+
+public class DailyEmployeeWage {
+
+	public static void main(String[] args) {
+				int wagePerHour = 20;
+				int fullDayHour = 8;
+				int dailyEmployeeWage = wagePerHour * fullDayHour;
+				
+				System.out.println("Daily Employee Wage is = "+dailyEmployeeWage);
+
+				}
+
+	}
+
+
