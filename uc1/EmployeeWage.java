1b3c5f8862b0c22564195b5495679b60a0674542
udaychopade27 <udaychopade27@gmail.com>
2023-12-20 08:56:20 +0530

    first commit for employeewage computation on master

---
+++ uc1/EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.uc1;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome to Employee Wage Computations");
+		// TODO Auto-generated method stub
+
+	}
+
+}
